56e006c48c9dd5a9ffecd73b3a1eb939fdeeacc1
ChrisZhang99 <33065656+ChrisZhang99@users.noreply.github.com>
2017-12-26 22:11:08 +0800

    send template message

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,7 +5,8 @@ Page( {
     cartImg: '../../images/cart-null.png',
     tipWords: '购物车空空如也',
     cartItems: [],
-    purchaseButtonDisabled: false
+    purchaseButtonDisabled: false,
+    access_token: null   
   },
 
   onLoad: function () {
@@ -28,6 +29,22 @@ Page( {
       purchaseButtonDisabled: (app.globalData.purchaseItems.length == 0)
     });
     console.log(that.data.cartItems);  
+
+    wx.request({
+      url: 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxb7d4a9bfe0803710&secret=d4f58b79a053947ef64b993d256e80fc',
+      method: 'get',
+      data: { },
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+        console.log("heheheehehehehe");
+        console.log(res);
+        that.setData({
+          access_token: res.data.access_token
+        });
+      }
+    })
   },
 
   placeOrder: function (e) {
@@ -49,8 +66,6 @@ Page( {
       url: 'https://www.snowcrane123.com/PurchaseOrders/Add/',
       method: 'POST',
       data: purchaseItemList,
-      //data: { "purchaseItems": JSON.stringify(app.globalData.purchaseItems) },
-      //data: {purchaseItems:purchaseItemList,creator:this.data.userInfo.baseInfo.nickName},
       dataType: "json",
       header: {
         'content-type': 'application/x-www-form-urlencoded'
@@ -62,7 +77,51 @@ Page( {
         that.setData({
           purchaseButtonDisabled: true
         });
+
+        var purchaseLines="";
+
+        for (var i = 0; i < res.data.data.PurchaseItems.length; i++) {
+
+          var line = res.data.data.PurchaseItems[i].productName + "     " + res.data.data.PurchaseItems[i].productNumber + "     " +
+          res.data.data.PurchaseItems[i].productUnit + "\r\n";
+          purchaseLines = purchaseLines + line;
+        }  
+        console.log(purchaseLines);
+        console.log("that.data.access_token");
+        console.log(that.data.access_token);
+        console.log("e.detail.formId");
+        console.log(e.detail.formId);
+        let url = 'https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=' + that.data.access_token;
+        let _jsonData = {
+          access_token: that.data.access_token,
+          touser: app.globalData.userInfo.openid,
+          template_id: 'gQiFCEFO-hR1Avof0joZQL7wdlxAJOFCmqToMJSZSWM',
+          form_id: e.detail.formId,
+          page: "pages/my/index",
+          data: {
+            "keyword1": { "value": res.data.data.PurchaseOrder.CreatedTime, "color": "#173177" },
+            "keyword2": { "value": purchaseLines, "color": "#173177" },
+          }
+        }
+
+        wx.request({
+          url: url,
+          data: _jsonData,
+          method: "POST",
+          success: function (res) {
+            console.log(res)
+          },
+          fail: function (err) {
+            console.log('request fail ', err);
+          },
+          complete: function (res) {
+            console.log("request completed!");
+          }
+
+        })
       }
     })
+
+    
   }
 })
