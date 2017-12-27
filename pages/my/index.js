fba8086fd9c42330dc363475e76687a663eae820
ChrisZhang99 <33065656+ChrisZhang99@users.noreply.github.com>
2017-12-27 15:13:09 +0800

    orders query

--- pages/my/index.js
+++ pages/my/index.js
@@ -8,10 +8,19 @@ Page( {
       text: '我的订单',
       isunread: true,
       unreadNum: 2
-    }],
+    },
+      {
+        id: 2,
+        icon: '../../images/purchase-orders.png',
+        text: '订单查询'
+      }],
     navRightItems: [],
     curMenuID: 1,
-    curMenuIndex: 0
+    curMenuIndex: 0,
+    startDate: '2016-11-08',
+    startTime: '12:00',
+    stopDate: '2016-11-08',
+    stopTime: '12:00'
   },
 
   onLoad: function() {
@@ -62,8 +71,6 @@ Page( {
         url: url + 'PurchaseOrders/GetByOwner',
         method: 'POST',
         data: { creator: this.data.userInfo.baseInfo.nickName },
-        //data: { "purchaseItems": JSON.stringify(app.globalData.purchaseItems) },
-        //data: {purchaseItems:purchaseItemList,creator:this.data.userInfo.baseInfo.nickName},
         dataType: "json",
         header: {
           'content-type': 'application/x-www-form-urlencoded'
@@ -77,5 +84,32 @@ Page( {
       })
     }
     
+  },
+
+  //  点击时间组件确定事件  
+  bindStartTimeChange: function (e) {
+    this.setData({
+      startTime: e.detail.value
+    })
+  },
+  //  点击日期组件确定事件  
+  bindStartDateChange: function (e) {
+    console.log(e.detail.value)
+    this.setData({
+      startDate: e.detail.value
+    })
+  },
+  //  点击时间组件确定事件  
+  bindStopTimeChange: function (e) {
+    this.setData({
+      stopTime: e.detail.value
+    })
+  },
+  //  点击日期组件确定事件  
+  bindStopDateChange: function (e) {
+    console.log(e.detail.value)
+    this.setData({
+      stopDate: e.detail.value
+    })
   }
 })
